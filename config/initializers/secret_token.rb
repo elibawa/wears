74a1c2637076bd7991327020dbcbaf2e13474a4c
kelvin bawa <elibawa@yahoo.com>
2013-06-18 06:48:45 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wears::Application.config.secret_token = 'cb66229d407de73cbdc605afb8ec77f2ccb0834106475213f79c00286a63d888d174ebc5658a38f785f9ddbe4f101894f22865bea65919bab53a9e42dd4d7c1e'
